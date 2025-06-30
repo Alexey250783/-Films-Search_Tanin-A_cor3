6e572bb862e3d860d6f3a72301dc19830cffdbf5
AlexeyTanin <tas@konti.su>
2025-06-30 22:56:36 +0300

    implemented parcelable

--- app/src/main/java/com/example/filmssearch3/domain/Film.kt
+++ app/src/main/java/com/example/filmssearch3/domain/Film.kt
@@ -1,15 +1,29 @@
 package com.example.filmssearch3.domain
 
+import android.annotation.SuppressLint
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
 import android.os.Parcel
 
-
+@SuppressLint("ParcelCreator")
 @Parcelize
 data class Film(
-    val title: String,
-    val poster: Int,
-    val description: String,
-    var rating: Float = 0f,
-    var isInFavorites: Boolean = false
-) : Parcelable
+   val title: String,
+   val poster: Int,
+   val description: String,
+   var rating: Float = 0f,
+   var isInFavorites: Boolean = false
+) : Parcelable {
+   override fun describeContents(): Int {
+      return 0
+   }
+
+   override fun writeToParcel(dest: Parcel, flags: Int) {
+      dest.writeString(title)
+      dest.writeInt(poster)
+      dest.writeString(description)
+      dest.writeFloat(rating)
+      dest.writeBoolean(isInFavorites)
+   }
+}
+
+annotation class Parcelize
